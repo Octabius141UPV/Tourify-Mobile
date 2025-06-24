70660e0cf104982ee791fd9852f6956027c358c9
Octabius141UPV <rforiba@etsinf.upv.es>
2025-06-24 11:16:04 +0200

    Implementa la integración de Google Sign-In en la aplicación, actualiza Info.plist para incluir la configuración necesaria y ajusta la versión mínima de iOS a 15.0. Se eliminan entradas innecesarias en el archivo de proyecto de Xcode.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import GoogleSignIn
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,7 +8,25 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    
+    // Configure Google Sign-In
+    if let path = Bundle.main.path(forResource: "GoogleService-Info", ofType: "plist"),
+       let plist = NSDictionary(contentsOfFile: path),
+       let clientId = plist["CLIENT_ID"] as? String {
+        guard let config = GIDConfiguration(clientID: clientId) else {
+            fatalError("Failed to configure Google Sign-In")
+        }
+        GIDSignIn.sharedInstance.configuration = config
+    }
+    
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+  
+  override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+    if GIDSignIn.sharedInstance.handle(url) {
+      return true
+    }
+    return super.application(app, open: url, options: options)
   }
 }
